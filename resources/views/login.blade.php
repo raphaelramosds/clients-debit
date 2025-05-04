b9e4942b3bdfb5bdcbe2f46aeb420e37dff587b4
Raphael Ramos <raphael.opensource@gmail.com>
2025-05-04 17:49:06 -0300

    feat: finish style of login and register page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,8 +1,8 @@
 <x-visitor-layout>
     <x-slot name="greeting">
-        <h1 class="text-[64px] font-bold">Bem vindo.</h1>
-        <p class="py-10">Ainda não tem uma conta?</p>
-        <a class="border-b-2 pb-1 px-2 font-bold" href="{{url('register')}}">Cadastre-se</a>
+        <h1 class="text-7xl font-bold">Bem vindo.</h1>
+        <p class="text-lg py-10">Ainda não tem uma conta?</p>
+        <a class="text-lg border-b-2 pb-1 px-2 font-bold" href="{{url('register')}}">Cadastre-se</a>
     </x-slot>
     <div>
         <x-application-logo :width="208" :height="36" />
@@ -13,7 +13,7 @@
             <x-input icon="fa-lock" placeholder="Senha"/>
         </div>
         <div class="mt-7">
-            <button class="bg-[#1B2845] text-white rounded-sm w-full py-2" type="submit">Entrar</button>
+            <button class="bg-[#1B2845] text-white rounded-[5px] w-full py-2" type="submit">Entrar</button>
         </div>
         <div class="mt-7 text-center text-[#555555]">
             <a href="#">Esqueci minha senha</a>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,12 +1,29 @@
 <x-visitor-layout>
     <x-slot name="greeting">
-        <h1 class="text-[64px] font-bold">Olá,</h1>
-        <p class="py-10">Preencha os campos ao lado para criar o seu cadastro.</p>
+        <h1 class="text-7xl font-bold">Olá,</h1>
+        <p class="text-lg py-10 text-left">Preencha os campos ao lado para criar <br> o seu cadastro.</p>
     </x-slot>
     <div>
-        Tela de Cadastro
+        <div class="mt-7">
+            <x-input icon="fa-user-circle" placeholder="Nome de usuário" />
+        </div>
+        <div class="mt-7">
+            <x-input icon="fa-envelope" placeholder="Email"/>
+        </div>
+        <div class="mt-7">
+            <x-input icon="fa-lock" placeholder="Senha"/>
+        </div>
+        <div class="mt-7">
+            <x-input icon="fa-lock" placeholder="Confirmar senha"/>
+        </div>
+        <div class="mt-7">
+            <button class="bg-[#1B2845] text-white rounded-[5px] w-full py-2" type="submit">Registrar-se</button>
+        </div>
     </div>
-    <div>
+    <div class="mt-7 text-center text-[#555555]">
+        <a href="{{url('login')}}">Esqueci minha senha</a>
+    </div>
+    <div class="mt-3 text-center text-[#555555]">
         <a href="{{url('login')}}">Voltar para a tela de login</a>
     </div>
 </x-visitor-layout>
